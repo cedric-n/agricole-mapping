b1b94864fa5dd79236194d35d4d4e44cf61f3d36
bloic <loic.benos@gmail.com>
2021-01-13 22:13:33 +0100

    map

---
+++ assets/map.js
@@ -0,0 +1,14 @@
+
+
+import L from 'leaflet';
+import 'devbridge-autocomplete';
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
