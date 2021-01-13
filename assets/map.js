ae7a1cebc89419864f6e337a4dfac7ed1e112e35
bloic <loic.benos@gmail.com>
2021-01-13 23:21:40 +0100

    add marker

--- assets/map.js
+++ assets/map.js
@@ -3,15 +3,15 @@
 import L from 'leaflet';
 import 'devbridge-autocomplete';
 
-let map = L.map('map').setView([46.232, 2.232], 13);
+let map = L.map('map').setView([48.852969, 2.349903], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var marker = L.marker([ 48.4438, 1.48814]).addTo(map);
-var marker = L.marker([ 48.4438, 1.414]).addTo(map);
-var marker = L.marker([ 48.4438, 1.47]).addTo(map);
+var marker = L.marker([ 45.4438, 1.414]).addTo(map);
+var marker = L.marker([ 49.4438, 1.47]).addTo(map);
 
 
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
