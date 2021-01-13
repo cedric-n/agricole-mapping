5388536d05ecf20f03b21882c9fa21c81b4abdd3
bloic <loic.benos@gmail.com>
2021-01-13 22:37:10 +0100

    add metaviewport

--- assets/map.js
+++ assets/map.js
@@ -3,12 +3,16 @@
 import L from 'leaflet';
 import 'devbridge-autocomplete';
 
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([46.232, 2.232], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
+
+var marker = L.marker([ 48.4438, 1.48814]).addTo(map);
+var marker = L.marker([ 48.4438, 1.414]).addTo(map);
+var marker = L.marker([ 48.4438, 1.47]).addTo(map);
 
 
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker.bindPopup("<b>Hello 1!</b><br>I am a popup.").openPopup();
